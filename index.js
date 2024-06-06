96c25a57bab3f6ff9bd43bb3eba962e49e71b54d
AraMaite <108091512+AraMaite@users.noreply.github.com>
2024-06-06 13:30:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector("#yesBtn");
 
 yesBtn.addEventListener("click",function () {
-    alert ("Sabia que ibas acerptarme de nuevo :3")
+    alert ("Sabia que ibas a querer '-'")
 });
 
 const noBtn = document.querySelector("#noBtn");
